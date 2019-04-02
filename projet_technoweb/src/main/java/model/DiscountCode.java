cd830f43b0c02dae42b79625ea29dbb1ef2ae2bf
cperrinc <cperrinc@10.183.103.16>
2019-04-02 14:26:30 +0200

    classe Dicount

---
+++ projet_technoweb/src/main/java/model/DiscountCode.java
@@ -0,0 +1,31 @@
+package model;
+
+/**
+ *
+ * @author cperrinc
+ */
+public class DiscountCode {
+
+    private String discountCode;
+
+    private float rate;
+
+    public DiscountCode(String discountCode, float rate) {
+        this.discountCode = discountCode;
+        this.rate = rate;
+    }
+
+    public String getDiscountCode() {
+        return discountCode;
+    }
+
+    public float getRate() {
+        return rate;
+    }
+
+    @Override
+    public String toString() {
+        return this.discountCode;
+    }
+
+}
